2e9ef8846dd25d4de63fc57bd17b5a95e0743bc9
sineptic <sineptic0@gmail.com>
2025-03-31 12:37:03 +0300

    add rehype-pretty-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { rehypePrettyCode } from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "catppuccin-mocha",
+        },
+      ],
+    ],
   },
 });
